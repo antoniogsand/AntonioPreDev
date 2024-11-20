59d491097b743091e90d8d510181d263fc5e024a
Antonio <antoniogs83@hotmail.com>
2024-11-20 19:09:46 +0100

    practica 6

--- 2024/Programacion 2024/constructor de copia/herencia/src/App.java
+++ 2024/Programacion 2024/constructor de copia/herencia/src/App.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
 /* 
-Constructor de copia hay que crear un objeto con un constructor normal y luego el constructor de copia crea una replica exacta de un objeto
+Constructor de copia hay que crear un objeto con un constructor normal
+luego el constructor de copia crea una replica exacta de un objeto
 
 */
 Cuadrado cuadradoUno= new Cuadrado("rojo", 4, 10, 12);
