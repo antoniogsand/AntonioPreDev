6ad5908d002124fd6409c073316ae46ede9ec53c
Antonio <tu.email@example.com>
2024-11-22 17:17:37 +0100

    tarea 6

--- 2024/Entornos de desarrollo/Tarea 6/tarea 6/src/App.java
+++ 2024/Entornos de desarrollo/Tarea 6/tarea 6/src/App.java
@@ -1,45 +1,31 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      
-      Dni dPrueba= new Dni();
-      System.out.println(dPrueba.generarDni());
-      
-      for (int i= 0; i<10; i++){
-        System.out.println(dPrueba.generarDni());
-      }
-      
-      
-      
-        /* 
-        Persona p1 = new Persona("Antonio", "García");
-        Dni d1 = new Dni("45740671T");
-        System.out.println(d1.validarDni());
+      ArrayList <Persona> personas= new ArrayList<>();
+      Random random = new Random();
+      String [] nombres= {"Maria", "Manuel", "Carmen", "Jose", "Ana", "Francisco","Laura", "David", "Pilar", "Juan", "Antonio", "Cristina"};
+      String [] apellidos= {"Garcia", "Rodriguez", "Fernandez", "Gonzalez", "Lopez", "Martinez", "Sanchez","Perez", "Gomez", "Martin", "Jimenez", "Hernandez"};
+    
+      for (int i = 0; i < 10; i++) {
+        String nombreAleatorio = nombres[random.nextInt(nombres.length)];
+        String apellidoAleatorio = apellidos[random.nextInt(apellidos.length)]; 
 
-      */
- /* 
-String cadenaAleatoria = generarCadenaAleatoria();
-        System.out.println(cadenaAleatoria);
-    
-    
+        Persona persona = new Persona(nombreAleatorio, apellidoAleatorio);
+        personas.add(persona);
+    }
+
+    // Imprimir las personas creadas (opcional)
+    for (Persona persona : personas) {
+        System.out.println(persona.getNombre() + " " + persona.getApellidos());
     }
     
-  private static String generarCadenaAleatoria() {
-        Random random = new Random();
-        StringBuilder cadena = new StringBuilder();
-
-        // Generar 8 dígitos aleatorios
-        for (int i = 0; i < 8; i++) {
-            cadena.append(random.nextInt(10));
-        }
-
-        // Generar una letra mayúscula aleatoria
-        char letraMayuscula = (char) (random.nextInt(26) + 'A');
-        cadena.append(letraMayuscula);
-
-        return cadena.toString();
- */
+    
+    
+    
+    
+    
     }
 }
