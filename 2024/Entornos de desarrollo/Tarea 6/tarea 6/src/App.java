22f24862c7c633ab15a896a58661d0107f3be2c8
Antonio <tu.email@example.com>
2024-11-24 19:36:09 +0100

    entornos 6 acabada

--- 2024/Entornos de desarrollo/Tarea 6/tarea 6/src/App.java
+++ 2024/Entornos de desarrollo/Tarea 6/tarea 6/src/App.java
@@ -8,65 +8,50 @@ public class App {
         // donde voy a guardar las personas creadas
     ArrayList <Persona> personas= new ArrayList<>();
     ArrayList <Dni> Dnis= new ArrayList<>();
-        // Esta clase tiene una método que genera un número aleatorio
+    Dni d1 = new Dni();
+        // La clase Dni tiene el método generarDni que genera un número aleatorio y le añade una letra al final
+        Random random = new Random();
+        
+        
         // En este caso me generará una posición aleatoria del array apellidos
         // Del otro array se puedem obtener las posiciones (del 0 al final) de forma secuencial con nombresSecuencial= nombres[i]
-        // Pero lo haré aleatorio también ya que quiero imprimir por pantalla más de 12 veces el contenido del array nombres
-    Dni d1 = new Dni();
-    
-    
-    Random random = new Random();
+        // Pero lo haré aleatorio también ya que quiero imprimir por pantalla más de 12 veces el contenido del array nombres    
     String [] nombres= {"Maria", "Manuel", "Carmen", "Jose", "Ana", "Francisco","Laura", "David", "Pilar", "Juan", "Antonio", "Cristina"};
     String [] apellidos= {"Garcia", "Rodriguez", "Fernandez", "Gonzalez", "Lopez", "Martinez", "Sanchez","Perez", "Gomez", "Martin", "Jimenez", "Hernandez"};
     
 
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 100; i++) {
         String nombreSecuencial =  nombres[random.nextInt(nombres.length)];
         String apellidoAleatorio = apellidos[random.nextInt(apellidos.length)]; 
 
         /* creo un objeto persona con dos String por parámetros como le indico en el constructor por parámetros
          * Se añaden los objetos al ArrayList
-         * las dos variables de tipo String solo están dentro del for pero el ArrayList está fuera y por eso se quedan almacenadas allí
+         * las dos variables de tipo String solo están dentro del for pero el ArrayList está fuera
+         * por eso se quedan almacenadas allí
+         * Creo un objeto de tipo Dni y usando el constructor por parámetros
+         * le entra un String que he creado antes y cuyo valor es el de un objeto tipo Dni junto
+         * con el método para generar un dni aleatorio
+         * al igual que con el ArrayLista de personas guardo en el ArrayList dnis los dnis generados
         */
         Persona persona = new Persona(nombreSecuencial, apellidoAleatorio);
         personas.add(persona);
-    }
-
-    for (int i=0; i<20; i++){
         String dniAleatorio= d1.generarDni();
         Dni dni= new Dni(dniAleatorio);
         Dnis.add(dni);
     }
-    // Generar personas más su dni
+
+    /* obtengo del ArrayList las personas generadas y los dni generados y le pongo como condición
+     * que solo me muestre las que tengan la letra A al final
+     */
     for (int i=0; i<personas.size(); i++) {
-                Persona persona = personas.get(i);
-                System.out.println(persona.getNombre() + " " + persona.getApellidos() + " " + d1.generarDni()); 
-        }
-
-    
-    // que solo muestre aquellas personas generadas cuyo dni acaba en A
-        for (int i=0; i<personas.size(); i++) {
-        if(d1.generarDni().charAt(8)== 'A'){
+        if(Dnis.get(i).getDni().charAt(8)== 'A'){
             Persona persona = personas.get(i);
-            System.out.println(persona.getNombre() + " " + persona.getApellidos() + " " + d1.getDni());
+            Dni dni= Dnis.get(i);
+            System.out.println(persona.getNombre() + " " + persona.getApellidos() + " " + dni.getDni()); 
+            }
         }
-    }
-    
 
-    
-    
-    
-    
-    
-    
     }
 
-
-
-
-
-
-
-    
 }
