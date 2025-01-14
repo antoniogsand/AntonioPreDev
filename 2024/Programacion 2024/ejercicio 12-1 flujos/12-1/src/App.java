04a58526563aa47ce97ec186b9b6c3892f72aafb
Antonio <tu.email@example.com>
2025-01-14 14:32:56 +0100

    ficheros

--- 2024/Programacion 2024/ejercicio 12-1 flujos/12-1/src/App.java
+++ 2024/Programacion 2024/ejercicio 12-1 flujos/12-1/src/App.java
@@ -1,9 +1,62 @@
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
     
+    
+        /* Realiza un programa que escriba en el fichero “personas.txt” ” con flujo de escritura carácter por carácter,
+        un ArrayList de 5 personas creado previamente y guarde cada persona en una línea del fichero.  */
+
+
+
+    ArrayList<Persona> listaPersonas= new ArrayList<Persona>();
+
+
+    Persona p1 = new Persona("Antonio", "Hombre");
+    Persona p2 = new Persona("María", "Mujer");
+    listaPersonas.add(new Persona("jose", "Hombre"));
+
+    listaPersonas.add(p1);
+    listaPersonas.add(p2);
+    
+    
+
+
+    FileWriter escritura = new FileWriter("listadoPersonas.txt");
+    
+
+
+
+
+    for (int i = 0; i<listaPersonas.size(); i++){
+        Persona obtenerPersona = listaPersonas.get(i);
+        String datosPersona = obtenerPersona.getNombre() + obtenerPersona.getSexo();
+        for (int j= 0; j<datosPersona.length(); j++){
+            escritura.write(datosPersona.charAt(j));
+        }
+        
+        
+        //escritura.write(personaString.charAt(i));
+
+        }
+    escritura.close();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        /* 
         String [] personas = new String[5];
 
         personas [0]= "Antonio";
@@ -14,12 +67,10 @@ public class App {
 
 
 
-        FileWriter escritura = new FileWriter("hola.txt");
     
-        for (int i = 0; i<personas.length; i++){
-            escritura.write(personas[i]);
-        }
-        escritura.close();
+
+         */
+
         /* 
         escritura.write("Una segunda prueba");
         FileReader lectura = new FileReader("Escribiendo.txt");
